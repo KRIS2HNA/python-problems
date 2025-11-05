bdfc5c0e6b96681d58d500630d24a97bc401e69f
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-05 23:39:28 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -339,4 +339,23 @@ elif op == '/':
     print("Result:", a / b)
 else:
     print("Invalid operator")
-58
+    
+    
+# 40 Simple calculator  
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))            
+operation = input("Enter operation (+, -, *, /): ")
+if operation == '+':
+    print("Result:", num1 + num2)
+elif operation == '-':
+    print("Result:", num1 - num2)
+elif operation == '*':
+    print("Result:", num1 * num2)
+elif operation == '/':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid operation")
+    
