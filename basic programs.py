d2b93500064ff988ff94d30e722f36069eea5ee8
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-08 22:58:46 +0530

    l 2

--- basic programs.py
+++ basic programs.py
@@ -417,13 +417,40 @@
 #             msb <<= 1
 #         return (1 << (msb.bit_length())) - 1 - self.minimumOneBitOperations(n ^ msb)
 
-# leetcode 1
+# # leetcode 1
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         num_map = {}
+#         for i, num in enumerate(nums):
+#             complement = target - num
+#             if complement in num_map:
+#                 return [num_map[complement], i]
+#             num_map[num] = i
+#         return []
+
+# leetcode 2
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-        return []
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy_head = ListNode(0)
+        current = dummy_head
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy_head.next
