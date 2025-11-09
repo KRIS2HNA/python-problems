d3cf4b171a20d479096cc8c719cb6ed5fcd1f2db
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-09 21:44:55 +0530

    l 2169

--- basic programs.py
+++ basic programs.py
@@ -430,27 +430,41 @@
 
 # leetcode 2
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+#         dummy_head = ListNode(0)
+#         current = dummy_head
+#         carry = 0
+
+#         while l1 or l2 or carry:
+#             val1 = l1.val if l1 else 0
+#             val2 = l2.val if l2 else 0
+#             total = val1 + val2 + carry
+#             carry = total // 10
+#             current.next = ListNode(total % 10)
+#             current = current.next
+
+#             if l1:
+#                 l1 = l1.next
+#             if l2:
+#                 l2 = l2.next
+
+#         return dummy_head.next
+
+
+# 2169 leetcode
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy_head = ListNode(0)
-        current = dummy_head
-        carry = 0
+    def countOperations(self, num1: int, num2: int) -> int:
+        ans = 0
+        while num1 and num2:
+            if num1 < num2:
+                num1, num2 = num2, num1
+            # we can do num1 // num2 subtractions in one go
+            ans += num1 // num2
+            num1 %= num2
+        return ans
 
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            total = val1 + val2 + carry
-            carry = total // 10
-            current.next = ListNode(total % 10)
-            current = current.next
-
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-
-        return dummy_head.next
