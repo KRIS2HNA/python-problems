df8ad1c9ad9eaf3f0587161a3f38775a6b24df47
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-09 21:46:48 +0530

    l 3

--- basic programs.py
+++ basic programs.py
@@ -468,3 +468,18 @@ class Solution:
             num1 %= num2
         return ans
 
+# 3
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        left = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            # If duplicate found and within current window
+            if s[right] in seen and seen[s[right]] >= left:
+                left = seen[s[right]] + 1
+            seen[s[right]] = right
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
