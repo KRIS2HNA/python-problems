e95406d3498532634bc451ef212b8ebb62267c59
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-11 18:03:31 +0530

    l 7

--- basic programs.py
+++ basic programs.py
@@ -583,3 +583,21 @@ class Solution:
                     
         return dp[m][n]
 
+# leetcode 7
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x_abs = abs(x)
+        rev = 0
+        
+        while x_abs != 0:
+            digit = x_abs % 10
+            rev = rev * 10 + digit
+            x_abs //= 10
+            
+        rev *= sign
+        
+        if rev < -2**31 or rev > 2**31 - 1:
+            return 0
+            
+        return rev
