260fa763dd777a25e51168a65db7f96a4b662e11
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-15 20:57:56 +0530

    l3234

--- basic programs.py
+++ basic programs.py
@@ -770,3 +770,14 @@ class Solution:
 
         return total
     
+    
+# 3234 leetcode
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s) - 2):
+            substring = s[i:i+3]
+            if len(set(substring)) == 3:
+                count += 1
+        return count
+    
