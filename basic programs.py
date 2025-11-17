10ad53b86153910564c40296e9f5ccacb25f5363
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-17 22:07:42 +0530

    prime number

--- basic programs.py
+++ basic programs.py
@@ -809,4 +809,15 @@ input_str = "Hello world"
 
 reverse_string = reverse_string(input_str)   
 
-
+# print prime numbers 
+def primr_numbers(n:int) -> list[int]:
+    primes = []
+    for num in range(2, n + 1):
+        for i in range(2, int(num ** 0.5)+ 1):
+            if num % i == 0:
+                break
+        else:
+            primes.append(num)
+    return primes
+prime_list = primr_numbers(100)
+print(prime_list)
