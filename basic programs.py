42b5dae23e55cd85b517ca4bba863ce08bf995fb
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-19 21:49:10 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -938,8 +938,12 @@ for i in range(n):
     print()
     
 
-
-    
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        s = set(nums)
+        while original in s:
+            original *= 2
+        return original
       
     
     
