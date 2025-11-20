4ac5f92389cc0cbd0cbc0eb9c53b2913403939f1
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-20 23:09:32 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -1021,6 +1021,16 @@ import heapq
 #         print("*", end = " ")
 #     print()
 
+# n = 7
+# for i in range(n):
+#     print(" " * (n - i - 1) + "*" * (2 * i + 1))
+
 n = 7
 for i in range(n):
-    print(" " * (n - i - 1) + "*" * (2 * i + 1))
+    print(" ", end = " ")
+    for j in range(2 * i + 1):
+        if j == 0 or j == 2 * i or i == n - 1:
+            print("*", end = "")
+        else:
+            print(" ", end = "")
+    print()
