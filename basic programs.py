bce91118859e65751afc6cb669a8667bf2c3e8dc
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-11-29 21:02:16 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -1383,5 +1383,14 @@ else:
     print(f'"{s}" is not a palindrome')
             
 
+# 19
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))  
+a, b = num1, num2
+while b != 0:   
+    a, b = b, a % b
+gcd = a
+print(f"GCD of {num1} and {num2} is {gcd}")
+
 
             
