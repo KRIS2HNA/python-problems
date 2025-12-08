03c4f7e3f38e5aa03b832be4453dc4031d2b57fa
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-12-08 23:29:40 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -1604,3 +1604,17 @@ class Solution:
             current_row += 1 if going_down else -1
 
         return ''.join(rows)
+    
+# # leetcode 474
+
+class Solution:       
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for s in strs:
+            zeros = s.count('0')
+            ones = s.count('1')
+            
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)   
