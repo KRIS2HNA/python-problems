417c5b1dae6439f9f1204ece05e0fd3161c26650
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-12-14 22:56:54 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -1873,3 +1873,31 @@ class Solution:
 
 #         # Return only coupon codes
 #         return [c for _, c in valid_coupons]
+
+class Solution:
+    def numberOfWays(self, corridor: str) -> int:
+        MOD = 10**9 + 7
+        n = len(corridor)
+
+        s = 0
+        for x in corridor:
+            if x == 'S': s += 1
+        if not s or s % 2 == 1: return 0
+
+        res = 1
+        l = 0
+        while l < n:
+            r = l
+            s = 0
+            while r < n and s < 2:
+                if corridor[r] == 'S': s += 1
+                r += 1
+
+            p = 0
+            while r < n and corridor[r] == 'P':
+                p += 1
+                r += 1
+
+            if r != n and p: res = (res * (p + 1)) % MOD
+            l = r
+        return res
