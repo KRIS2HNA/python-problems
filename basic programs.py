f430e417cac686e9f31048ad737c8b70b2660076
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-12-18 00:03:55 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -1993,3 +1993,14 @@ Exit
 #     factorial *= i
 # print(f"THe factorial of {num} is {factorial}")
 
+# print list of numbers in ascending order
+n = int(input("Enter the number of elements:"))
+numbers = []
+for i in range(n):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+numbers.sort()
+print("Numbers in ascending order:")
+for num in numbers:
+    print(num)  
+    
