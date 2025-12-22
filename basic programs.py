eccc852255d6f063decf26cb8c6e3f57df99604e
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2025-12-22 19:33:40 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2077,3 +2077,21 @@ print(res)
 #                 break
 
 #     return count
+
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        cols = len(strs[0])
+        rows = len(strs)
+        dp = [1] * cols
+
+        for c1 in range(cols -2, -1, -1):
+            for c2 in range(c1 + 1, cols):
+                valid = True
+                for r in range(rows):
+                    if strs[r][c1] > strs[r][c2]:
+                        valid = False
+                        break
+
+                if valid: dp[c1] = max(dp[c1], 1 + dp[c2])
+
+        return cols - max(dp)
