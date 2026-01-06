8f2f59618fe710abd0451521f3a3b9eab69cb8a2
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-06 21:27:12 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2707,3 +2707,31 @@ Exit
 #         series.append(a)
 #     print("The fibonacci series is:", series)
 # feb(10)
+
+## Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        mx = -inf
+        res = 0
+        level = 1
+
+        while q:
+            total = 0
+            for _ in range(len(q)):
+                n = q.popleft()
+                total += n.val
+                if n.left: q.append(n.left)
+                if n.right: q.append(n.right)
+
+            if total > mx:
+                mx = total
+                res = level
+            level += 1
+
+        return res
