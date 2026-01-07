8c69cc51f1b0a1918a2684871819102da2a5eba4
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-07 15:50:24 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2736,13 +2736,13 @@ Exit
 
 #         return res
 
-def pattern(n):
-    for i in range(n):
-        for j in range(n):
-            if i==0 or j==0 or i==n or i==n//2:
-                print("*", end = ' ')
-            else:
-                print(" ", end = " ")
+# def pattern(n):
+#     for i in range(n):
+#         for j in range(n):
+#             if i==0 or j==0 or i==n or i==n//2:
+#                 print("*", end = ' ')
+#             else:
+#                 print(" ", end = " ")
                 
-        print()
-pattern(7)
+#         print()
+# pattern(7)
