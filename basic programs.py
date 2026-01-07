cab10bc2947bc3a56205767f170f89645f319625
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-07 15:27:52 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2714,24 +2714,24 @@ Exit
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        q = deque([root])
-        mx = -inf
-        res = 0
-        level = 1
+# class Solution:
+#     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+#         q = deque([root])
+#         mx = -inf
+#         res = 0
+#         level = 1
 
-        while q:
-            total = 0
-            for _ in range(len(q)):
-                n = q.popleft()
-                total += n.val
-                if n.left: q.append(n.left)
-                if n.right: q.append(n.right)
+#         while q:
+#             total = 0
+#             for _ in range(len(q)):
+#                 n = q.popleft()
+#                 total += n.val
+#                 if n.left: q.append(n.left)
+#                 if n.right: q.append(n.right)
 
-            if total > mx:
-                mx = total
-                res = level
-            level += 1
+#             if total > mx:
+#                 mx = total
+#                 res = level
+#             level += 1
 
-        return res
+#         return res
