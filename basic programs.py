6b785b123c3002a61a631dd9a5e59b4f2b54efa5
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-08 17:13:26 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2746,13 +2746,13 @@ Exit
                 
 #         print()
 # pattern(7)
-outer = [[]] * 3
-outer[0].append(1)
-print(outer)
+# outer = [[]] * 3
+# outer[0].append(1)
+# print(outer)
 
-n= 4
-results = []
-for i in range(1, n+1):
-    for _ in range(i):
-        results.append([i])
-print(results)
+# n= 4
+# results = []
+# for i in range(1, n+1):
+#     for _ in range(i):
+#         results.append([i])
+# print(results)
