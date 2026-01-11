0f1685aff29ac6c5ea29c65d82f3c9112b97dddd
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-11 12:33:56 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2812,3 +2812,10 @@ Exit
 
 #                     )
 #         return dp[-1][-1]
+
+
+for num in range(1, 1001):
+    power = len(str(num))
+    total = sum(int(d)**power for d in str(num))
+    if total == num:
+        print(num)
