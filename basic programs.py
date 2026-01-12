8cad34a604dd1d6b8ea054ea9fb1c9130d6c8452
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-12 21:46:20 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2826,24 +2826,24 @@ Exit
 #     if total == num:
 #         print(num)
         
-def pattern(n):
-    for i in range(1, n + 1):
-        for j in range(1, n * 2):
-            if j == i or j == (2 * n - i):
-                print("*", end= '')
-            else:
-                print(" ", end= '')
-        print()
-pattern(5)
+# def pattern(n):
+#     for i in range(1, n + 1):
+#         for j in range(1, n * 2):
+#             if j == i or j == (2 * n - i):
+#                 print("*", end= '')
+#             else:
+#                 print(" ", end= '')
+#         print()
+# pattern(5)
 
-# print v pattern
-def pattern(n):
-    for i in range(1, n + 1):
-        for j in range(1, n * 2):
-            if j == i or j == (2 * n - i):
-                print("*", end = " ")
-            else:
-                print(" ", end = " ")
-        print()
-pattern(4)
+# # print v pattern
+# def pattern(n):
+#     for i in range(1, n + 1):
+#         for j in range(1, n * 2):
+#             if j == i or j == (2 * n - i):
+#                 print("*", end = " ")
+#             else:
+#                 print(" ", end = " ")
+#         print()
+# pattern(4)
 
