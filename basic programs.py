71980254159406c7382a7cc2bb7b1df1c6bf5304
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-12 21:59:27 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2847,3 +2847,17 @@ Exit
 #         print()
 # pattern(4)
 
+def pattern(n):
+    for i in range(0, n):
+        for j in range(0, n):
+            if i == 0 or i == n//2 or i == n-1:
+                print("*", end = " ")
+            elif i < n//2 and j == 0:
+                print("*", end = " ")
+            elif i > n//2 and j == n-1:
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")
+        print()
+        
+pattern(8)
