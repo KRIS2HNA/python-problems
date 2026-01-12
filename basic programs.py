ca33ad87a06904a21b540bd518d64969ea96a16a
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-12 21:45:54 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2819,3 +2819,31 @@ Exit
 #     total = sum(int(d)**power for d in str(num))
 #     if total == num:
 #         print(num)
+
+# for num in range(1, 1999):
+#     power = len(str(num))
+#     total = sum(int(d) ** power for d in str(num))
+#     if total == num:
+#         print(num)
+        
+def pattern(n):
+    for i in range(1, n + 1):
+        for j in range(1, n * 2):
+            if j == i or j == (2 * n - i):
+                print("*", end= '')
+            else:
+                print(" ", end= '')
+        print()
+pattern(5)
+
+# print v pattern
+def pattern(n):
+    for i in range(1, n + 1):
+        for j in range(1, n * 2):
+            if j == i or j == (2 * n - i):
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")
+        print()
+pattern(4)
+
