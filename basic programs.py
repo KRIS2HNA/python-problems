911ac7f166615f3ee9237613a4befd528d62ce4e
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-14 22:58:14 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2868,8 +2868,8 @@ Exit
 
 n = 4
 
-for i in range(1, n + 1):
-    print(" " * (n - i) + "* " * i)
+# for i in range(1, n + 1):
+#     print(" " * (n - i) + "* " * i)
 
-for i in range(n - 1, 0, -1):
-    print(" " * (n - i) + "* " * i)
+# for i in range(n - 1, 0, -1):
+#     print(" " * (n - i) + "* " * i)
