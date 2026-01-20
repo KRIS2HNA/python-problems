b2040ba123c5697902e1428d9a1b77530c851138
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-01-20 18:58:58 +0530

    python

--- basic programs.py
+++ basic programs.py
@@ -2956,41 +2956,41 @@ n = 4
 #                 right = mid - 1
 #         return ans
 
-class Solution:
-    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        m, n = len(mat), len(mat[0])
+# class Solution:
+#     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+#         m, n = len(mat), len(mat[0])
 
-        pref = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m):
-            for j in range(n):
-                pref[i + 1][j + 1] = (
-                    mat[i][j]
-                    + pref[i][j+1]
-                    + pref[i + 1][j]
-                    - pref[i][j]
-                )
+#         pref = [[0] * (n + 1) for _ in range(m + 1)]
+#         for i in range(m):
+#             for j in range(n):
+#                 pref[i + 1][j + 1] = (
+#                     mat[i][j]
+#                     + pref[i][j+1]
+#                     + pref[i + 1][j]
+#                     - pref[i][j]
+#                 )
 
-        def can(k):
-            for i in range(m - k + 1):
-                for j in range(n - k + 1):
-                    total = (
-                        pref[i + k][j + k]
-                        - pref[i][j + k]
-                        - pref[i + k][j]
-                        + pref[i][j]
+#         def can(k):
+#             for i in range(m - k + 1):
+#                 for j in range(n - k + 1):
+#                     total = (
+#                         pref[i + k][j + k]
+#                         - pref[i][j + k]
+#                         - pref[i + k][j]
+#                         + pref[i][j]
 
-                    )
-                    if total <= threshold:
-                        return True
+#                     )
+#                     if total <= threshold:
+#                         return True
 
-            return False
+#             return False
 
-        left, right, ans = 0, min(m, n), 0
-        while left <= right:
-            mid = (left + right) // 2
-            if can(mid):
-                ans = mid
-                left = mid + 1
-            else:
-                right = mid - 1
-        return ans
+#         left, right, ans = 0, min(m, n), 0
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if can(mid):
+#                 ans = mid
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+#         return ans
